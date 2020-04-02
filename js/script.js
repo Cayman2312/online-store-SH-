01ccc5ff173beb23750059e1d684314ba7878a4d
M.Studitskiy <cayman2312@gmail.com>
2020-04-02 15:16:00 +0300

    добавил раскрытие/скрытие меню по нажатию на бургер

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@
  * Общие js функции
  * 
  */
+document.querySelector('.navbar-toggle').addEventListener('click', function () {
+  let navbarMenu = document.querySelector('.navbar-menu');
+  navbarMenu.classList.toggle('open');
+})
