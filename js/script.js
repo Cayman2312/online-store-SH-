b5a6b660560c6c383674bda3330ed255b41b2a4f
San_e4ek <alexandr.keeper@yandex.ru>
2020-04-04 16:17:43 +0300

    сверстал и завалидировал popup для авторизации, наложил анимацию и ховеры на index.php, добавил js к header

--- js/script.js
+++ js/script.js
@@ -1,9 +1,81 @@
 /**
- * 
+ *
  * Общие js функции
- * 
+ *
  */
 document.querySelector('.navbar-toggle').addEventListener('click', function () {
-  let navbarMenu = document.querySelector('.navbar-menu');
-  navbarMenu.classList.toggle('open');
+    let navbarMenu = document.querySelector('.navbar-menu');
+    navbarMenu.classList.toggle('open');
 })
+
+
+// Клики на меню ---------------------------
+
+const $navItems = document.querySelectorAll('.menu-item');
+
+$navItems.forEach(function (item) {
+
+    if (item.classList.contains('active')) {
+        item.classList.remove('active');
+    }
+
+    item.addEventListener('click', function () {
+
+        this.classList.add('active');
+        // debugger
+    });
+});
+
+//------------------------------------------
+
+// Popup-login и валидация -----------------
+
+const $logIn = document.querySelector('.user-box__login');
+const $popupLog = document.querySelector('.popup-log');
+const $logForm = document.querySelector('.popup-log__form');
+
+
+$logIn.addEventListener('click', function () {
+    if ($popupLog.classList.contains('open')) {
+        $popupLog.classList.remove('open');
+        $popupLog.style.opacity = 0;
+        $errorPass.style.opacity = 0;
+        $errorLogin.style.opacity = 0;
+        $logForm.reset();
+    } else {
+        $popupLog.classList.add('open');
+        $popupLog.style.opacity = 1;
+    }
+});
+
+const $errorLogin = document.querySelector('.error-login');
+const $errorPass = document.querySelector('.error-pass');
+
+function checkForm(form) {
+    let e = 0;
+
+    for (let i = 0; i < form.length - 1; i++) {
+
+        if (!form[i].value.replace(/^\s+|\s+$/g, '') && i == 0) {
+            $errorLogin.style.opacity = 1;
+        } else if (!form[i].value.replace(/^\s+|\s+$/g, '') && i == 1) {
+            $errorPass.style.opacity = '1';
+        }
+
+        if (form[i].value.replace(/^\s+|\s+$/g, '') && i == 0) {
+            $errorLogin.style.opacity = 0;
+        } else if (form[i].value.replace(/^\s+|\s+$/g, '') && i == 1) {
+            $errorPass.style.opacity = 0;
+        }
+
+        if (form[0].value.replace(/^\s+|\s+$/g, '') && form[1].value.replace(/^\s+|\s+$/g, '')) {
+            e = 1;
+        }
+    }
+
+    if (!e) {
+        return false
+    }
+}
+
+//------------------------------------------
--- parts/header.php
+++ parts/header.php
@@ -44,4 +44,16 @@ include('parts/header_conf.php');
                 <a href="#" class="user-box__login">Войти</a>
                 <a href="/basket.php" class="user-box__basket">Корзина</a>
             </div>
+            <div class="popup-log">
+                <div class="error-login"><img src="/images/icons/warning.png">поле не заполнено</div>
+                <div class="error-pass"><img src="/images/icons/warning.png">поле не заполнено</div>
+                <form method="get" action="/#" class="popup-log__form" onsubmit="return checkForm(this.elements)">
+                    <input type="email" name="login" placeholder="E-mail">
+                    <input type="password" name="pass" placeholder="Password">
+                    <input type="submit" value="Войти">
+                </form>
+                <span><a href="#/">забыли пароль?</a></span>
+                <span>/</span>
+                <span><a href="#/">регистрация</a></span>
+            </div>
         </header>
