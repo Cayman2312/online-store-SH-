b66b49fc14de0de8fa3fa405b9d53258f43d00ce
M.Studitskiy <cayman2312@gmail.com>
2020-04-07 10:49:23 +0300

    добавил вывод размеров в соответствии с таблицей product_sizes, переделал карточку товара на form, добавил анимацию к checkbox

--- product.php
+++ product.php
@@ -19,6 +19,7 @@ $template = [
     'name' => '',
     'description' => '',
     'price' => '',
+    'sizes' => []
 ];
 
 if (!empty($_GET['id'])) {
@@ -29,12 +30,19 @@ if (!empty($_GET['id'])) {
     $sql = "SELECT * FROM products WHERE id='{$_GET['id']}'";
     $result = mysqli_query($link, $sql);
     $template = mysqli_fetch_assoc($result);
+
+    $sql_sizes = "SELECT product_sizes FROM product_sizes WHERE product_id='{$_GET['id']}'";
+    $result_sizes = mysqli_query($link, $sql_sizes);
+    $row = mysqli_fetch_assoc($result_sizes);
+    $template['sizes'] = json_decode($row['product_sizes']);
 } else {
     header('Location: /catalog.php');
+
+
 }
 ?>
 
-<div class="product">
+<form action="#" class="product">
     <div class="product-img" style="background-image: url(<?= $template['img_url'] ?>)"></div>
     <div class="product-name"><?= $template['name'] ?></div>
     <div class="product-article">артикул <?= $template['id'] ?></div>
@@ -42,16 +50,15 @@ if (!empty($_GET['id'])) {
     <div class="product-description"><?= $template['description'] ?></div>
 
     <div class="product-size">
-        <a href="#/">32</a>
-        <a href="#/">32</a>
-        <a href="#/">32</a>
-        <a href="#/">32</a>
-        <a href="#/">32</a>
-        <a href="#/">32</a>
+        <?php while (count($template['sizes']) > 0) : ?> 
+        <?php $current_size = array_shift($template['sizes'])?>           
+            <input id="<?= 'size'.$current_size ?>" type="checkbox" class="product-size__checkbox" value="<?= $current_size ?>">
+            <label for="<?= 'size'.$current_size ?>"><?= $current_size ?></label>        
+        <?php endwhile ; ?>
     </div>
 
-    <div class="product-btn" data-product-id="<?= $template['id'] ?>">Добавить в корзину</div>
-</div>
+    <input type="submit" class="product-btn" value="Добавить в корзину" data-product-id="<?= $template['id'] ?>">
+</form>
 
 <?php
 $footer_config = [
