aa6b6a238363590458dab0bc3b2cfb9c295878f3
San_e4ek <alexandr.keeper@yandex.ru>
2020-04-12 01:53:50 +0300

    Довел до ума авторизацию! Обновил базу и оптимизировал код

--- parts/header.php
+++ parts/header.php
@@ -1,5 +1,7 @@
 <?php
 
+$auth_page = true;
+
 include('parts/header_conf.php');
 
 //подсчет количества элементов в корзине
@@ -13,6 +15,16 @@ if (isset($_SESSION['basket']) && !empty($_SESSION['basket'])) {
 //получение списка категорий для отрисовки на странице
 $sql_category_list = "SELECT * FROM categories";
 $result_category_list = mysqli_query($link, $sql_category_list);
+
+// авторизация
+
+$is_auth = isset($_SESSION['is_auth']) && $_SESSION['is_auth'];
+$is_admin = isset($_SESSION['is_admin']) && $_SESSION['is_admin'];
+$user_name = $_SESSION['user_name'];
+
+if (!$is_auth && (!isset($auth_page) || !$auth_page)) {
+    header('Location: /');
+}
 
 ?>
 
@@ -43,33 +55,53 @@ $result_category_list = mysqli_query($link, $sql_category_list);
             </div>
             <div class="navbar-menu">
                 <?php while ($category = mysqli_fetch_assoc($result_category_list)) : ?>
-                    <a href="/catalog.php?category_id=<?= $category['id'] ?>" class="menu-item"><?= $category['name'] ?></a>
-                <?php endwhile ; ?>
+                    <a href="/catalog.php?category_id=<?= $category['id'] ?>"
+                       class="menu-item"><?= $category['name'] ?></a>
+                <?php endwhile; ?>
             </div>
         </nav>
+
         <div class="header__user-box">
-            <div class="user-box__login">Войти</div>
+            <div class="user-box__login">
+                <?php if (!$is_auth) : ?>
+                    Войти
+                <?php endif; ?>
+                <?php if ($is_auth) : ?>
+                    <?= $user_name ?>
+                <?php endif; ?>
+            </div>
+
             <a href="/basket.php" class="user-box__basket">
                 Корзина (<b><?= $basket_sum ?></b>)
             </a>
         </div>
+
         <div class="popup-log">
             <div class="popup-log__close"></div>
-            <form method="GET" action="/#" class="popup-log__form"
-                  onkeyup="return checkFormLog(this.elements)">
-                <div class="form__group">
-                    <input type="email" name="email" placeholder=" ">
-                    <label>Email</label>
-                </div>
-                <div class="form__group">
-                    <input type="password" name="password" placeholder=" ">
-                    <label>Пароль</label>
-                </div>
-                <input type="submit" value="Войти" disabled>
-            </form>
-            <span><a href="#/" class="forgot-href">забыли пароль?</a></span>
-            <span>/</span>
-            <span><a href="#" class="reg-href">регистрация</a></span>
+            <?php if (!$is_auth) : ?>
+                <form method="GET" class="popup-log__form"
+                      onkeyup="return checkFormLog(this.elements)">
+                    <div class="form__group">
+                        <input type="email" name="email" placeholder=" ">
+                        <label>Email</label>
+                    </div>
+                    <div class="form__group">
+                        <input type="password" name="password" placeholder=" ">
+                        <label>Пароль</label>
+                    </div>
+                    <input type="submit" value="Войти" disabled>
+                </form>
+                <span><a href="#/" class="forgot-href">забыли пароль?</a></span>
+                <span>/</span>
+                <span><a href="#" class="reg-href">регистрация</a></span>
+            <?php endif; ?>
+
+            <?php if ($is_auth) : ?>
+                <?php if ($is_admin) : ?>
+                    <a href="/admin/" class="href-admin">Админка</a>
+                <?php endif; ?>
+                <a href="/exit.php" class="href-exit">Выйти</a>
+            <?php endif; ?>
         </div>
 
         <!-- Попап уведомлений -->
@@ -88,7 +120,7 @@ $result_category_list = mysqli_query($link, $sql_category_list);
 
         if (!empty($_GET['login']) && !empty($_GET['pass'])) {
 
-//            exit("<meta http-equiv='refresh' content='0; url= /'>");
+            //            exit("<meta http-equiv='refresh' content='0; url= /'>");
         }
     }
 
