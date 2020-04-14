833669925084b20d2bb8c92221baf4aecc77809c
M.Studitskiy <cayman2312@gmail.com>
2020-04-14 09:53:13 +0300

    добавил возможность редактирования категорий товара в админке с обновлением базы

--- admin/js/product_edit.js
+++ admin/js/product_edit.js
@@ -6,6 +6,31 @@ document.addEventListener("DOMContentLoaded", function (e) {
     document.querySelector('.card-img-top').src = this.value;
   })
 
+  //проверка наличия категорий
+  let $productCategory = document.querySelector('.product_category');
+  let productCategory = $productCategory.dataset.productCategory;
+  let $inputProductCategory = document.querySelectorAll('.custom-control-input');
+
+  if (!isNaN(productCategory)) {
+    for (let $input of $inputProductCategory) {
+      if ($input.dataset.category == productCategory) {
+        $input.checked = true;
+      }
+    }
+  } else {
+    productCategory = productCategory.split(',');
+    console.log(productCategory);
+    for (let $input of $inputProductCategory) {
+      for (let item of productCategory) {
+        if ($input.dataset.category == item) {
+          $input.checked = true;
+          break;
+        }
+      }
+    }
+  }
+
+  //проверка типа товара
   const $select = document.getElementById('inlineFormCustomSelect');
   const $options = $select.getElementsByTagName('option');
   for (let i = 0; i < $options.length; i++) {
